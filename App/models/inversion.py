35972e9d3e6ab03620aac3a6426d26ab57cbdde8
Ale27654 <ale208406@gmail.com>
2026-01-19 12:30:00 -0500

    Add Inversion class with final value calculation

--- App/models/inversion.py
+++ App/models/inversion.py
@@ -1,1 +1,7 @@
+class Inversion:
+    def __init__(self, monto, rendimiento):
+        self.monto = monto
+        self.rendimiento = rendimiento
 
+    def valor_final(self):
+        return self.monto * (1 + self.rendimiento)
