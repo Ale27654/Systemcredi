a483776a451fedb149831db022dc478424fa27c7
Ale27654 <ale208406@gmail.com>
2026-01-19 12:38:53 -0500

    Add investment recommendation function

--- App/service/recomendador_inversion.py
+++ App/service/recomendador_inversion.py
@@ -1,1 +1,7 @@
-
+def recomendar_inversion(capacidad_ahorro):
+    if capacidad_ahorro <= 0:
+        return "No se recomienda invertir en este momento."
+    elif capacidad_ahorro < 200000:
+        return "Inversión conservadora (ahorro programado)."
+    else:
+        return "Inversión moderada (fondos o CDT)."
